d0298211619961a7b954914bac67e5abb59082f5
keuvyntdev <keuvyntdev@gmail.com>
2025-03-08 11:42:51 -0300

    feat: ensure sale creation is done with a transaction

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -6,51 +6,54 @@ import { revalidatePath } from "next/cache";
 
 export const createSale = async (data: CreateSaleSchema) => {
   createSaleSchema.parse(data);
-  const sale = await db.sale.create({
-    data: {
-      date: new Date(),
-    },
-  });
-  for (const product of data.products) {
-    // Consulta o produto no back-end
-    const productFromDb = await db.product.findUnique({
-      where: {
-        id: product.id,
+  await db.$transaction(async (trx) => {
+    const sale = await trx.sale.create({
+      data: {
+        date: new Date(),
       },
     });
-
-    // Valida se o produto existe
-    if (!productFromDb) {
-      throw new Error("Product not found");
-    }
-
-    // Valida se a quantidade informada existe em estoque
-    const productIsOutOfStock = product.quantity > productFromDb.stock;
-    if (productIsOutOfStock) {
-      throw new Error("Product out of stock");
-    }
-    await db.saleProduct.create({
-      data: {
-        saleId: sale.id,
-        productId: product.id,
-        quantity: product.quantity,
-
-        // Obtem o preço do back-end
-        unitPrice: productFromDb.price,
-      },
-    });
-
-    // Atualiza a quantidade em estoque no banco de dados
-    await db.product.update({
-      where: {
-        id: product.id,
-      },
-      data: {
-        stock: {
-          decrement: product.quantity,
+    for (const product of data.products) {
+      // Consulta o produto no back-end
+      const productFromDb = await db.product.findUnique({
+        where: {
+          id: product.id,
         },
-      },
-    });
-  }
+      });
+  
+      // Valida se o produto existe
+      if (!productFromDb) {
+        throw new Error("Product not found");
+      }
+  
+      // Valida se a quantidade informada existe em estoque
+      const productIsOutOfStock = product.quantity > productFromDb.stock;
+      if (productIsOutOfStock) {
+        throw new Error("Product out of stock");
+      }
+      await trx.saleProduct.create({
+        data: {
+          saleId: sale.id,
+          productId: product.id,
+          quantity: product.quantity,
+  
+          // Obtem o preço do back-end
+          unitPrice: productFromDb.price,
+        },
+      });
+  
+      // Atualiza a quantidade em estoque no banco de dados
+      await trx.product.update({
+        where: {
+          id: product.id,
+        },
+        data: {
+          stock: {
+            decrement: product.quantity,
+          },
+        },
+      });
+    }
+  })
+  
   revalidatePath("/products");
 };
